3f9f3a247de17ee0ce887e38d69fee9fa99f4d37
Brian Ogilvie <brian.ogilvie@story2.com>
2019-11-20 12:09:23 -0500

    set up starter code

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const authHandler = require('./handlers/authHandler');
-const router = require('./routes');
+const Product = require('./models/Product');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.use(authHandler);
-app.use(router);
 
 app.listen(3000, () => {
   console.log('Nile app is listening on Port 3000');
--- routes/reviewsRouter.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-
-const reviewsRouter = Router();
-
-reviewsRouter.use('*', (req, res) => res.send('Reviews!'));
-
-module.exports = reviewsRouter;
